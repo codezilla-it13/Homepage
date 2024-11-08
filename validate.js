f693ec0c6c3e17df884de9cb096f9f392625fcf3
codezilla-it13 <faizulmd1306@gmail.com>
2024-11-08 19:40:56 +0530

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const correct_btn = document.getElementById("correct-btn");
+    correct_btn.addEventListener('click', function(){
+        window.location.href="index.html";
+    })
+
+    const wrong_btn = document.getElementById("wrong-btn");
+    wrong_btn.addEventListener('click', function(){
+        document.getElementById("response").innerHTML = "Only CSE Department is Available";
+    })
+})
